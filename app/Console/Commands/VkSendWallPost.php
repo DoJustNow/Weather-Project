adbca2d63afbe396a816f3422cd925fcbcc07dda
Alexey <slivkinalexey20@gmail.com>
2018-11-25 01:40:39 +0300

    Добавлены команды

---
+++ app/Console/Commands/VkSendWallPost.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Weather;
+use Exception;
+use Illuminate\Console\Command;
+use Log;
+use VK\Client\VKApiClient;
+
+class VkSendWallPost extends Command
+{
+
+    private $vkApiMethods;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'wall:post';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Публикация поста на стену личной страницы VK с прогнозом погды';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //VK Sdk
+        $vk           = new VKApiClient();
+        $access_token = env('VK_ACCESS_TOKEN');
+        //Число отправляемых записей о прогнозе погоды в одном посте
+        $numberForecastSent = 4;
+        do {
+            usleep(340);
+            $weathers = Weather::where('posted', 0)
+                               ->orderBy('created_at')
+                               ->limit($numberForecastSent)
+                               ->get();
+            if ($weathers->isEmpty()) {
+                die("Новых записей для публикации нет.\n");
+            }
+            $message = '';
+            //Перебор записей погоды из БД; reverse чтобы они шли снизу вверх по дате
+            foreach ($weathers->reverse() as $weather) {
+                //Формирование сообщения поста
+                $message .= "Date: $weather->created_at " .
+                            "City: $weather->city " .
+                            "Тemp: $weather->temperature " .
+                            "Weather: $weather->condition " .
+                            "Wind: $weather->wind_speed " .
+                            "API: $weather->api\n\n";
+            }
+            //Публикация поста
+            try {
+                $result = $vk->wall()
+                             ->post($access_token, ['message' => $message]);
+            } catch (Exception $exception) {
+                Log::error($exception->getMessage());
+                $this->error($exception->getMessage());
+                return;
+            }
+
+            //Назначаем id опубликованного поста записям
+            foreach ($weathers as $weather) {
+                $weather->posted = $result['post_id'];
+                $weather->save();
+            }
+            $this->info("ID поста: $result[post_id]");
+        } while ($weathers->count() == $numberForecastSent);
+
+    }
+}
---
+++ app/Console/Commands/VkSyncWallPosts.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Weather;
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection;
+use VK\Client\VKApiClient;
+
+class VkSyncWallPosts extends Command
+{
+
+    private $postsIdBuffer;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'wall:post:sync';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Комманда выполняющая синхронизацию постов VK.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $vk             = new VKApiClient();
+        $access_token   = env('VK_ACCESS_TOKEN');
+        $numberGetPosts = 100;
+        $wallPosts      = Weather::select('posted')
+                                 ->where('posted', '!=', 0)
+                                 ->orderBy('created_at')
+                                 ->chunk($numberGetPosts/*TODO*/,
+                                     function (Collection $posts) {
+                                         foreach ($posts as $post) {
+                                             $this->postsIdBuffer[]
+                                                 = $post->posted;
+                                         }
+                                     });
+        //Получили ID всех уникальных постов за все время
+        $postsId = array_unique($this->postsIdBuffer);
+        //dd($postsId);
+        //dd($this->postsIdBuffer);
+        dump($postsId);
+        //Цикл по постам в вк//TODO
+        $response = $vk->wall()
+                       ->get($access_token,
+                           [
+                               'filter' => 'owner',
+                               'count'  => 100,
+                               'offset' => 0,
+                           ]);
+        //Цикл по полученным постам из VK
+        foreach ($response['items'] as $item) {
+            //Если на стене есть пост c ID из базы данных удаляем его из буфера т.к он активен
+            //По итогу в массиве остануться только ID постов которые небыли найдены на стене
+            if (($key = array_search($item['id'], $postsId)) !== false) {
+                unset($postsId[$key]);
+            }
+        }
+        Weather::whereIn('posted', $postsId)
+               ->chunk($numberGetPosts, function (Collection $weathers) {
+                   foreach ($weathers as $weather) {
+                       $weather->posted = 0;
+                       $weather->save();
+                   }
+               });
+
+    }
+}
