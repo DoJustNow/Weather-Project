a79d85a54ac1aa96c95b11a73193ed8732cb0ce8
Alexey <slivkinalexey20@gmail.com>
2018-12-16 23:59:34 +0300

    Vk скрипт вынесен в head

--- resources/views/feedback/vk.blade.php
+++ resources/views/feedback/vk.blade.php
@@ -1,11 +1,9 @@
-<script src="https://vk.com/js/api/openapi.js?160" type="text/javascript"></script>
 <script type="text/javascript">
     VK.init({
         apiId: 6743977,
         onlyWidgets: true
     });
 </script>
-
 <div class="container">
     <div class="justify-content-center">
         <div id="vk_comments"></div>
@@ -15,6 +13,6 @@
     VK.Widgets.Comments('vk_comments', {
         limit: 7,
         autoPublish: 1,
-        pageUrl: 'weather-service.mchost.ru/feedback'
+        pageUrl: 'weather-service.mcdir.ru/feedback/vk'
     });
 </script>
