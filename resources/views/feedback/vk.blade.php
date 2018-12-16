75ac3ea89cc4d2e3325c70830bd4c9059df97d93
Alexey <slivkinalexey20@gmail.com>
2018-12-17 00:26:14 +0300

    Изменен способ инициализации VK виджета комментариев с обычного на асинхронный

--- resources/views/feedback/vk.blade.php
+++ resources/views/feedback/vk.blade.php
@@ -1,18 +1,25 @@
 <script type="text/javascript">
-    VK.init({
+    window.vkAsyncInit = function() {
+        VK.init({
         apiId: 6743977,
         onlyWidgets: true
-    });
+        });
+        VK.Widgets.Comments('vk_comments', {
+            limit: 7,
+            autoPublish: 1,
+            pageUrl: 'weather-service.mcdir.ru/feedback/vk'
+        });
+    };
+    setTimeout(function() {
+        var el = document.createElement("script");
+        el.type = "text/javascript";
+        el.src = "https://vk.com/js/api/openapi.js?160";
+        el.async = true;
+        document.getElementById("vk_comments").appendChild(el);
+    }, 0);
 </script>
 <div class="container">
     <div class="justify-content-center">
         <div id="vk_comments"></div>
     </div>
 </div>
-<script type="text/javascript">
-    VK.Widgets.Comments('vk_comments', {
-        limit: 7,
-        autoPublish: 1,
-        pageUrl: 'weather-service.mcdir.ru/feedback/vk'
-    });
-</script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,6 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/wow.min.js') }}"></script>
-    <script src="https://vk.com/js/api/openapi.js?160" type="text/javascript"></script>
     <script>new WOW().init();</script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
